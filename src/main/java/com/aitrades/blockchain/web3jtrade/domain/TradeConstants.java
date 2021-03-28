ee981d25e75ee4446097305b8fad8515253cd1ef
Padimala Jagadesh <22383480+pjagadish86@users.noreply.github.com>
2021-03-28 13:23:22 -0400

    uod

--- src/main/java/com/aitrades/blockchain/web3jtrade/domain/TradeConstants.java
+++ src/main/java/com/aitrades/blockchain/web3jtrade/domain/TradeConstants.java
@@ -7,7 +7,6 @@ import com.google.common.collect.ImmutableMap;
 public final class TradeConstants {
 	
 	public static final String UNISWAP = "UNISWAP";
-
 	public static final String UNISWAP_FACOTRYADDRESS ="0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 	public static final String UNISWAP_ROUTERADDRESS ="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 	public static final String UNISWAP_WETH_FACOTRYADDRESS ="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
@@ -23,16 +22,16 @@ public final class TradeConstants {
 	public static final String SUSHI_ROUTER_ADDRESS = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
 	public static final String SUSHI_WETH_FACOTRYADDRESS ="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 	
-	public static final String SNIPETRANSACTIONREQUEST = "SNIPETRANSACTIONREQUEST";
 	public static final String ORDER_DECISION = "ORDER_DECISION";
-	
+
+	public static final String SNIPETRANSACTIONREQUEST = "SNIPETRANSACTIONREQUEST";
     public static final String SWAP_ETH_FOR_TOKEN_HASH = "SWAP_ETH_FOR_TOKEN_HASH";
 	public static final String APPROVE_HASH = "APPROVE_HASH";
 	public static final String PAIR_CREATED = "PAIR_CREATED";
 	public static final String HAS_RESERVES = "HAS_RESERVES";
 	public static final String HAS_LIQUIDTY_EVENT = "HAS_LIQUIDTY_EVENT";
-	public static final String OUTPUT_TOKENS = "OUTPUT_TOKENS";
 	
+	public static final String OUTPUT_TOKENS = "OUTPUT_TOKENS";
 	public static final String INPUT_TOKENS = "INPUT_TOKENS";
 	
 	public static final Map<String, String> ROUTER_MAP = ImmutableMap.of(UNISWAP, UNISWAP_ROUTERADDRESS, PANCAKE, PANCAKE_ROUTER_ADDRESS, SUSHI, SUSHI_ROUTER_ADDRESS);
@@ -40,7 +39,6 @@ public final class TradeConstants {
 	public static final Map<String, String> FACTORY_MAP = ImmutableMap.of(UNISWAP, UNISWAP_FACOTRYADDRESS, PANCAKE, PANCAKE_FACTORY_ADDRESS, SUSHI, SUSHI_FACTORY_ADDRESS);
 	
 	public static final Map<String, String> WETH_MAP = ImmutableMap.of(UNISWAP, UNISWAP_WETH_FACOTRYADDRESS, PANCAKE, PANCAKE_WETH_FACOTRYADDRESS, SUSHI, SUSHI_WETH_FACOTRYADDRESS);
-	
 	
 	public static final String APPROVE_HASH_ISAVAILABLE = "APPROVE_HASH_ISAVAILABLE";
 
